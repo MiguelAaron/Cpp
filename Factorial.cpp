09275d221d347f40f082eae3f13a0c68e383a861
MiguelAaron <AaronXYZ.96@gmail.com>
2017-01-23 14:02:22 -0600

    otro

---
+++ Factorial.cpp
@@ -0,0 +1,26 @@
+#include<math.h>
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+  int N = 0;
+  int F = 1;
+  int U = N;
+
+  cout<<"Escribe un numero: ";
+  cin>>N;
+  
+  while(U > 1)
+  {
+    F = F*U;
+    U--;
+  }
+  
+  cout<<"El factorial de ";
+  cout<<N;
+  cout<<" es: ";
+  cout<<F<<endl;
+}
